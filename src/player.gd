ca921bfae95c58386a309cf2ab183d1486d6d051
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2023-12-03 19:26:50 +0000

    player movement done
    Sprites required were also implemented.

--- src/player.gd
+++ src/player.gd
@@ -1,7 +1,21 @@
 extends Node2D
 
-@onready var path_ref : Path2D = $Path2D
-@onready var body_ref : CharacterBody2D = $CharacterBody2D
+@onready var path_ref : PathFollow2D = $Path2D/PathFollow2D
 
-@export  var speed := 20
+@export var speed := 20
+@export var acceleration := 100
+@export var friction := 50
 
+var directional_input := Vector2.ZERO
+var curve_progress := Vector2.ZERO
+
+
+func _physics_process(delta):
+    directional_input.x = Input.get_action_strength("Move_Right") - Input.get_action_strength("Move_Left")
+
+    if directional_input.x != 0:
+        curve_progress = curve_progress.move_toward(directional_input * speed, acceleration * delta)
+    else:
+        curve_progress = curve_progress.move_toward(Vector2.ZERO, friction * delta)
+    
+    path_ref.progress = path_ref.progress + curve_progress.x
