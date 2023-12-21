de46cdad101c234edb23f7ad70d7ffbf334443cc
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2023-12-21 02:36:37 +0000

    vignette mask WIP

--- src/main.gd
+++ src/main.gd
@@ -2,13 +2,17 @@ extends Node
 
 @onready var midi_player : MidiPlayer = $MidiPlayer
 @onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer
-
 var is_fullscreen := false
 
-# Test
+# TestShake
 var strength = 0.5;
 var curStrength = 0;
 @onready var screen_shake = $ScreenShake
+
+# TestVideo
+var mask_radius := 0.0
+var my_tween
+@onready var background_sprite : Sprite2D = $Background
 
 
 func _ready():
@@ -20,17 +24,50 @@ func _ready():
 func _process(delta):
 	# Probably this should be in a function in another scene
 	curStrength = max(curStrength - delta * 2, 0); # - delta * NUMBER --> ratio of shake to stop
-	if(Input.is_action_just_pressed("Move_Left")):
+	if(Input.is_action_just_pressed("move_Left")):
 		curStrength = strength;
 	
 	#screen_shake.get_child(0).material.set_shader_parameter("ShakeStrength", max(curStrength,0))
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Escape"):
+	if event.is_action_pressed("escape"):
 		if is_fullscreen:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			is_fullscreen = false
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 			is_fullscreen = true
+
+	if event.is_action_pressed("more_radius"):
+		handle_mask_radius(true)
+
+	if event.is_action_pressed("less_radius"):
+		handle_mask_radius(false)
+
+
+func handle_mask_radius(increase: bool):
+
+	var modifier := 1.0
+	if !increase:
+		modifier = -1.0
+
+	var cache_radius := mask_radius
+	mask_radius += 0.1 * modifier 
+	mask_radius = clamp(mask_radius, 0.001, 0.3)
+
+	var my_tween2 = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	my_tween2.tween_method(set_mask_radius.bind(background_sprite), cache_radius, mask_radius, 1.0)
+
+	if my_tween == null:
+		pass
+		#my_tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	else:
+		pass
+		#print("Stuff")
+		#var my_tween2 = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+		#my_tween2.tween_method(set_mask_radius.bind(background_sprite), cache_radius, mask_radius, 1.0)
+
+
+func set_mask_radius(new_radius: float, sprite: Sprite2D):
+	sprite.material.set_shader_parameter("MULTIPLIER", new_radius)
--- src/spawner.gd
+++ src/spawner.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var outer_circle = $OuterCircle
 var note_spawners
 
+#TestVar
+var process_all_channels = true
+
 
 func _ready():
 	SignalManager.note_on.connect(Shoot)
@@ -10,6 +13,13 @@ func _ready():
 
 
 func Shoot(channel, _event, note, _velocity):
-	if channel.number in [0, 1, 2]:
-		note_spawners[note%12].Shoot()
+
+	if process_all_channels:
+		if channel.number in [0, 1, 2]:
+			note_spawners[note%12].Shoot()
+			if channel.number == 0:
+				process_all_channels = false
+	else:
+		if channel.number == 0:
+			note_spawners[note%12].Shoot()
 		
