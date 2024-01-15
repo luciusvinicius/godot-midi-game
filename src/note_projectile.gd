288e5e56d18d66525d30b37b28c32e403e6c3d81
Lucius Vinicius <luciusviniciusf@ua.pt>
2024-01-15 20:44:39 +0000

    clean code + remove ProjectileBlock on main

--- src/note_projectile.gd
+++ src/note_projectile.gd
@@ -2,87 +2,38 @@ extends Node2D
 
 const CIRCLE_LENGTH = 390
 const BASE_SCORE = 100
-const BASE_SPEED = 800
-const INITIAL_SPEED_RATE = 0.75 # % of final speed (0.75 looks good)
-var max_speed : float # Defined on the time of animation, on NoteSpawner scene.
+var SPAWN_OFFSET
 @onready var score_audio = $ScoreAudio
 @onready var sprite_ref : Sprite2D = $Sprite
+var acceleration := 0.0
+# 230 speed ~= 383 midi duration
+# 460 speed ~= 192 midi duration
+var speed
 
 var give_points := false
 
-# Cosine movement (rads)
-#@onready var initial_speed = INITIAL_SPEED_RATE * BASE_SPEED * max_speed
-#@onready var acceleration = _calculate_acceleration(initial_speed, BASE_SPEED * max_speed)
-var acceleration := 0.0
-const ACCELERATION_MULTIPLIER = 1.25
-var initial_speed := 100.0
-#@onready var speed := initial_speed
-
-
-
-# 230 speed ~= 383 midi duration
-# 460 speed ~= 192 midi duration
-var test_speed
-var bpm = 190.0
-
-func setup(duration):
-	var new_duration = 1.2 #duration/bpm #(duration * 1/8) - 80 # Magic shenanigans
-	acceleration = _get_acceleration(new_duration)
-	max_speed = _get_max_speed(new_duration)
-	print(acceleration)
 
 func calculate_speed(duration: int):
-	test_speed = (-1.204 * duration) + 660
-
-#func _process(delta):
-	#speed += acceleration * delta
-	#position += Vector2.UP * speed * delta # Direction has rotation of parent node in consideration
-	#
-
+	speed = (-1.204 * duration) + 691.204
 
 func _process(delta):
-	test_speed += acceleration * delta
-	position += Vector2.UP * test_speed * delta # Direction has rotation of parent node in consideration
-	
-	# When cross the line
-	if position.y < -CIRCLE_LENGTH and acceleration == 0:
-		acceleration = -_calculate_acceleration(test_speed)
-		#queue_free()
-	
-	# Convert to a point before starting to go back
-	elif test_speed < 0 and not give_points: turn_into_point()
-	
-	# Projectile hit the center --> ban
-	elif position.y > -100 and acceleration != 0: queue_free()
-	
-
-func _calculate_acceleration(speed):
-	return speed # Have no idea why this works perfectly, but we take those
-
-""" func _process(delta):
 	speed += acceleration * delta
 	position += Vector2.UP * speed * delta # Direction has rotation of parent node in consideration
 	
-	# Invert when passed center
-	if speed > max_speed * BASE_SPEED:
-		acceleration *= -ACCELERATION_MULTIPLIER
+	# When cross the line
+	if position.y < -CIRCLE_LENGTH and acceleration == 0:
+		acceleration = -_calculate_acceleration(speed)
 	
 	# Convert to a point before starting to go back
 	elif speed < 0 and not give_points: turn_into_point()
 	
 	# Projectile hit the center --> ban
-	elif speed < -BASE_SPEED * max_speed * ACCELERATION_MULTIPLIER: queue_free() """
+	elif position.y > -SPAWN_OFFSET and acceleration != 0: queue_free()
 
+# Top 10 funções foda
+func _calculate_acceleration(speed):
+	return speed # Have no idea why this works perfectly, but we take those lmao
 
-# --- || Formulas || ---
-
-func _get_acceleration(time, distance:=250.0):
-	# Using formula from accelerated movement a = 2(S - Vot) / t², if So == 0:
-	return 2 * (distance - initial_speed*time) / time**2
-
-func _get_max_speed(time):
-	# Using V = Vo + at
-	return initial_speed + acceleration * time
 
 # --- || Points || ---
 
